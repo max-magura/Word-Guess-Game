0fd30a8ea6cef855a2780de1cd73f886e0b4b90c
Max-M <maxnmagura@gmail.com>
2019-08-08 22:08:18 -0500

    pulled word array loop out of onkey function, created variables to pull innerhtml out to give wins and guesses, and pulled the guessesletterArray out of the for loop to stop it showing multiple letters when guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,7 @@
-var wins = 0;
-var guesses = 10;
+var numWins = 0;
+var numGuesses = 10;
 var guessesLettersArray = [];
 var guessesLettersArrayObject;
-
-var wins = document.getElementById("wins");
-var guesses = document.getElementById("guesses-number");
-var guessesLettersArrayObject = document.getElementById("guesses-letters");
-var currentWordArrayObject = document.getElementById("current-word");
 
 var wordChoices = [ "michael", "dwight", "scranton", "jim", "pam", "creed", "jan", "ryan", "oscar", "erin", "andy", "beets", "bears", "angela", "meredith", "stanley", "sabre", "dunder", "mifflin", "daryl", "kelly", "toby", "kevin", "tots", "dundies", "athlead", "surplus", "stutter", "benihana", "bankruptcy"
 ];
@@ -16,38 +11,46 @@ var currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 var currentWordArray = [];
 var currentWordArrayObject
 
+wins = document.getElementById("wins");
+wins.innerHTML = numWins;
+guesses = document.getElementById("guesses-number");
+guesses.innerHTML = numGuesses;
+guessesLettersArrayObject = document.getElementById("guesses-letters");
+currentWordArrayObject = document.getElementById("current-word");
+
+//needs to be initlized somehow//
+for (var i = 0; i < currentWord.length; i++) {
+  currentWordArray[i] = "_";
+}
+
 document.onkeyup = function(event) {
 
   var userGuess = event.key;
   console.log (currentWord)
 
   //fill in the current word with spaces for the current word letters//
-    for (var i = 0; i < currentWord.length; i++) {
-      currentWordArray[i] = "_";
-    }
-
+    
     if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
 
       for (var j = 0; j < currentWord.length; j++) {
         if (currentWord[j] === userGuess) {
         currentWordArray[j] = userGuess;
-        guesses--;
-        }
-
-        else {
-          guessesLettersArray.push(userGuess);
         }
 
       }
+      guessesLettersArray.push(userGuess);
+
+      numGuesses--;
     }
 
     if (currentWord === wordChoices) {
-      wins++
+      numWins++
     }
 
-  wins.textContent = wins;
-  guesses.textContent = guesses;
-  guessesLettersArrayObject.textContent = guessesLettersArray;
-  currentWordArrayObject.textContent = currentWordArray;
+
+    wins.innerHTML = numWins;
+    guesses.innerHTML = numGuesses;
+    guessesLettersArrayObject.textContent = guessesLettersArray;
+    currentWordArrayObject.textContent = currentWordArray; 
 
 }
