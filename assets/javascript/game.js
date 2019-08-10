8b0524cad71db8b830bccbd136128221a80fa595
Max-M <maxnmagura@gmail.com>
2019-08-09 22:14:21 -0500

    added in onload function for window called setup and then included that in the win 'if' statement in order to correctly reload the elements besides win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,7 @@ var numWins = 0;
 var numGuesses = 10;
 var guessesLettersArray = [];
 var guessesLettersArrayObject;
-
-
-
+var currentWord
 var currentWordArray = [];
 var currentWordArrayObject
 
@@ -15,7 +13,9 @@ guesses.innerHTML = numGuesses;
 guessesLettersArrayObject = document.getElementById("guesses-letters");
 currentWordArrayObject = document.getElementById("current-word");
 
-var wordChoices = [ "michael", "dwight", "scranton", "jim", "pam", "creed", "jan", "ryan", "oscar", "erin", "andy", "beets", "bears", "angela", "meredith", "stanley", "sabre", "dunder", "mifflin", "daryl", "kelly", "toby", "kevin", "tots", "dundies", "athlead", "surplus", "stutter", "benihana", "bankruptcy"
+window.onload = function setup() {
+
+ var wordChoices = [ "michael", "dwight", "scranton", "jim", "pam", "creed", "jan", "ryan", "oscar", "erin", "andy", "beets", "bears", "angela", "meredith", "stanley", "sabre", "dunder", "mifflin", "daryl", "kelly", "toby", "kevin", "tots", "dundies", "athlead", "surplus", "stutter", "benihana", "bankruptcy"
   ];  
   
 var currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
@@ -24,13 +24,10 @@ for (var i = 0; i < currentWord.length; i++) {
   currentWordArray[i] = "_";
 }
 
-
-var correctWord = currentWord
-
 document.onkeyup = function game(event) {
 
   var userGuess = event.key.toLocaleLowerCase();
-  console.log (correctWord)
+  console.log (currentWord)
     
     if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
 
@@ -55,10 +52,14 @@ document.onkeyup = function game(event) {
       var winning = true
       numGuesses=10;
       guessesLettersArray=[];
+      currentWordArray=[]
+      alert("Correct! The word was " + correctAnswer)
+      setup();
     }
 
     wins.innerHTML = numWins;
     guesses.innerHTML = numGuesses;
     guessesLettersArrayObject.textContent = guessesLettersArray;
     currentWordArrayObject.textContent = currentWordArray; 
+}
 }
